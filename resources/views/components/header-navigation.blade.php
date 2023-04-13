ec13557b984383a5de47cdbd364650189542cd80
jmarcos16 <contato.marcosrf@gmail.com>
2023-04-13 15:21:14 -0300

    Refactory layout default

---
+++ resources/views/components/header-navigation.blade.php
@@ -0,0 +1,19 @@
+<div class="fixed bg-white text-black h-[4.4rem] w-full border flex items-center justify-between px-5 z-20">
+    <div class="flex items-center m-0 w-3/6">
+        {{-- <i class='bx bx-menu text-3xl'></i> --}}
+        <a href="#" class="flex items-center ml-5 text-5xl">
+            <i class='bx bxl-tailwind-css text-blue-500'></i>
+            <span class="text-2xl font-semibold">Theman</span>
+        </a>
+        <div class="hidden lg:flex ml-10 w-3/6 items-center ">
+            <i class='bx bx-search translate-x-9 text-lg text-[#6B7280]'></i>
+            <input type="text" class="border-slate-300 placeholder-slate-400 px-10 bg-gray-50 rounded-lg w-4/5 py-2"
+                placeholder="Search">
+        </div>
+    </div>
+
+    <div>
+        LOGIN
+    </div>
+
+</div>
